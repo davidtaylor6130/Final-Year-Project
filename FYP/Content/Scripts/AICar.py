972f25164dfe2f1a1597f960eb1f7b96fd7961d1
David Taylor <43450525+davidtaylor6130@users.noreply.github.com>
2020-12-29 22:57:55 +0000

    Need to fix self error

--- FYP/Content/Scripts/AICar.py
+++ FYP/Content/Scripts/AICar.py
@@ -1,55 +1,53 @@
 import tensorflow as tf
 import unreal_engine as ue
-
-#from tensorflow import Keras
 from TFPluginAPI import TFPluginAPI
 
 class CarAi(TFPluginAPI):
 
     #Simmilar To Constructor 
-    #def AiConstructor(self):
-    #    # Declaring Required Variable
-    #    self.RunsTillSave = 5
-    #    self.RunsCompleated = 0
-    #    self.ShutDownActive = False
+    def CarAiConstructor(self):
+        # Declaring Required Variable
+        self.RunsTillSave = 5
+        self.RunsCompleated = 0
+        self.ShutDownActive = False
 
-    #    #Building The Neural Network's Structure
-    #    self.model = Keras.models.Sequential()
-    #    self.model.add(Keras.Layers.Dense(16, activation='relu', input_shape=(8,)))
-    #    self.model.add(Keras.Layers.Dense(16, activation='relu'))
-    #    self.model.add(Keras.Layers.Dense(2, activation='sigmoid'))
+        #Building The Neural Network's Structure
+        self.model = tf.keras.models.Sequential()
+        self.model.add(tf.keras.layers.Dense(16, activation='relu', input_shape=(8,)))
+        self.model.add(tf.keras.layers.Dense(16, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(2, activation='sigmoid'))
 
         #Loading the Last Weights Back in
-    #    self.LoadModel(self)
+        self.LoadModel()
 
     #Contains Neural Network Input and Outputs
-    #def AiUpdate(self):
-    #    outputs = {0,0}
-    #    return outputs
+    def AiUpdate():
+        outputs = {0,0}
+        return outputs
 
     #Contains The Optimisation of the weights and calculation of loss score
-    #def AiDeath(self):
-    #    self.RunsCompleated += 1
+    def AiDeath():
+        self.RunsCompleated += 1
 
     #Saves Current Weights Everytime incase of pc crashes then saves after every 5 deaths as a backup
-    #def SaveModel(self):
-    #    self.model.save_weights("CurrentWeights.h5")
-    #    if (self.RunsTillSave == 0):
-    #        fileName = "Trained " + self.RunsCompleated + ".h5"
-    #        self.model.save_weight(fileName)
-    #    else:
-    #        self.RunsTillSave -= 1
+    def SaveModel():
+        self.model.save_weights("CurrentWeights.h5")
+        if (self.RunsTillSave == 0):
+            fileName = "Trained " + self.RunsCompleated + ".h5"
+            self.model.save_weight(fileName)
+        else:
+            self.RunsTillSave -= 1
 
     #loads the last save to the network
-    #def LoadModel(self):
-    #    self.model.load_weights("CurrentWeights.h5")
+    def LoadModel():
+        self.model.load_weights("CurrentWeights.h5")
 
 
 
 
 #//------------------------------------ Python API Functions -----------------------------------//
     def onSetup(self):
-        self.AiConstructor(self)
+        self.CarAiConstructor(self)
 
     #Parse Json to useable Data
     def onJsonInput(self, jsonInput):
